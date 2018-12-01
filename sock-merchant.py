042419eed18947c4f34f2457bf2fa49ab737f0d9
Ricardo Leon <ricardoleoncorreia@gmail.com>
2018-12-01 15:10:34 -0300

    Fix sock merchant bug

--- sock-merchant.py
+++ sock-merchant.py
@@ -4,18 +4,13 @@ from itertools import groupby
 # Sock merchant
 def sockMerchant(n, ar):
 
-    # Checking constrains
-    while n < 1 or n > 100:
-        print('You must select a number between 1 and 100')
-        n = int(input('Number of single socks: '))
-    while (len(ar) is not n) or (max(ar) > 100) or (min(ar) < 1):
-        print('The number of socks must be {} and colors must be between 1 and 100'.format(n))
-        ar = list(map(int, input('Socks colors ({} single socks): '.format(n)).rstrip().split()))
 
     # Calculate sock pairs
     socks_pairs = 0
-    for key, group in groupby(ar):
-        socks_pairs += floor(len(list(group)) / 2)
+    sock_colors = set(ar)
+    for color in sock_colors:
+        socks_pairs += floor(ar.count(color) / 2)
+        
     return socks_pairs
 
 
@@ -25,10 +20,20 @@ if __name__ == '__main__':
 
     n = int(input('Number of single socks: '))
 
+    # Checking constrains
+    while n < 1 or n > 100:
+        print('You must select a number between 1 and 100')
+        n = int(input('Number of single socks: '))
+    
     ar = list(map(int, input('Socks colors ({} single socks): '.format(n)).rstrip().split()))
+    
+    # Checking constrains
+    while (len(ar) is not n) or (max(ar) > 100) or (min(ar) < 1):
+        print('The number of socks must be {} and colors must be between 1 and 100'.format(n))
+        ar = list(map(int, input('Socks colors ({} single socks): '.format(n)).rstrip().split()))
 
     result = sockMerchant(n, ar)
 
-    fptr.write(str(result) + '\n')
+    fptr.write('For n={} and ar={}, John can sell {} pairs'.format(n, ar, result))
 
     fptr.close()
